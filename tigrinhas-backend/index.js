aeaa1fac62738f1fbc43da5751bba431dd253a9a
Frutyger <csar.frutyger@gmail.com>
2025-06-20 00:43:20 -0300

    Configuração inicial do backend com Express

---
+++ tigrinhas-backend/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Servidor backend funcionando');
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
