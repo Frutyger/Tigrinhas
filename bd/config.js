2f705120a95bb1db19e13122b1a20e77e33df4b0
Frutyger <csar.frutyger@gmail.com>
2025-06-20 00:45:16 -0300

    Configuração inicial do banco de dados com PostgreSQL

---
+++ bd/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  development: {
+    username: 'seu_usuario',
+    password: 'sua_senha',
+    database: 'tigrinhas',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+  },
+};
